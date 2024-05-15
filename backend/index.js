cb53c44563b1265182dfdde35098157f258934e6
Ayush Pandey <116146717+ayush-pandey1@users.noreply.github.com>
2024-05-15 17:31:35 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const app = express();
 require("dotenv").config();
 
 app.use(cors({
-  origin: "https://nivesh-netra.vercel.app",
+  origin: "*",
   methods: ["GET", "POST"], // Add any other methods you need
   credentials: true // Enable credentials (cookies, authorization headers, etc.)
 }));
