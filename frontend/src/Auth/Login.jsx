2be28c3cc088a883aecf3fc1e770bad18309144d
Ayush Pandey <116146717+ayush-pandey1@users.noreply.github.com>
2024-05-15 15:35:04 +0530

    Update Login.jsx

--- frontend/src/Auth/Login.jsx
+++ frontend/src/Auth/Login.jsx
@@ -9,7 +9,6 @@ import { loginRoute } from "../utility/APIRoutes.js";
 
 function Login() {
   const navigate=useNavigate();
-  axios.defaults.withCredentials = true;
 
   useEffect(() => {
     if (localStorage.getItem("NiveshNetra-app")) {
